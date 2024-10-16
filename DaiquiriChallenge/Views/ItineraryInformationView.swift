5da0341967af5c2c96465476a4bde820adf4d633
Dilyorbek Sharofiddinov <142020869+dilyorbeck@users.noreply.github.com>
2024-10-17 00:16:23 +0200

    Created ItineraryInformationView.swift

---
+++ DaiquiriChallenge/Views/ItineraryInformationView.swift
@@ -0,0 +1,35 @@
+//
+//  ItineraryInformationView.swift
+//  DaiquiriChallenge
+//
+//  Created by Luca Maria Incarnato on 16/10/24.
+//
+
+import SwiftUI
+
+struct ItineraryInformationView: View {
+    var duration: Int
+    var itinerary: Itinerary
+    
+    var body: some View {
+        if(!itinerary.activities.isEmpty){
+            ForEach(0..<(2 * duration + 1), id: \.self) { index in
+                Text("Day \(index + 1)")
+                    .font(.title2)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                ForEach(itinerary.activities){ activity in
+                    if(activity.day == index + 1){
+                        ActivityCardView(activity: activity)
+                            .padding([.leading, .bottom, .trailing])
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ItineraryInformationView(duration: 0, itinerary: Itinerary(name: "Naples", duration: 1, activities: [Activity(name: "Prova 1", hourDuration: 1, day: 1, tips: "None")]))
+}
