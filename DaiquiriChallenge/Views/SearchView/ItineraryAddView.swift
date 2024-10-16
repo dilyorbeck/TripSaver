79d926cbf3d5d5296db32770333fbbe74c781bc5
Dilyorbek Sharofiddinov <142020869+dilyorbeck@users.noreply.github.com>
2024-10-17 00:12:23 +0200

    Update ItineraryAddView.swift

--- DaiquiriChallenge/Views/SearchView/ItineraryAddView.swift
+++ DaiquiriChallenge/Views/SearchView/ItineraryAddView.swift
@@ -1,3 +1,10 @@
+//
+//  ItineraryAddView.swift
+//  DaiquiriChallenge
+//
+//  Created by Luca Maria Incarnato on 13/10/24.
+//
+
 import SwiftUI
 
 struct ItineraryAddView: View {
@@ -9,13 +16,12 @@ struct ItineraryAddView: View {
     @State var isAdded = false
     
     var body: some View {
-        NavigationStack {
+        NavigationStack{
             ZStack {
                 Color.gray
                     .opacity(0.1)
                     .ignoresSafeArea()
-                
-                VStack {
+                VStack{
                     Picker("Select period", selection: $duration) {
                         Text("1 day").tag(0)
                         Text("3 days").tag(1)
@@ -23,41 +29,35 @@ struct ItineraryAddView: View {
                         Text("7 days").tag(3)
                     }
                     .pickerStyle(.segmented)
-                    .padding(.top)
+                    .padding(.vertical)
                     .padding(.horizontal)
                     .task(id: duration) {
                         isAdded = false
                     }
-                    VStack {
-                        switch duration {
-                        case 0: OneDayView()
-                        case 1: ThreeDaysView()
-                        case 2: FiveDaysView()
-                        case 3: SevenDaysView()
-                        default: Text("There's no itinerary for this period")
-                        }
+                    ScrollView{
+                        ItineraryInformationView(duration: duration, itinerary: destination.itinerariesAvailable[duration])
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     Button(isAdded ? "Added" : "Add to \"Your trips\"") {
                         if duration < destination.itinerariesAvailable.count {
-                            myData.yourItineraries.append(Itinerary(name: destination.itinerariesAvailable[duration].name, duration: destination.itinerariesAvailable[duration].duration))
+                            myData.yourItineraries.append(Itinerary(name: destination.itinerariesAvailable[duration].name, duration: destination.itinerariesAvailable[duration].duration, activities: destination.itinerariesAvailable[duration].activities))
                             isAdded = true
                         }
                     }
-                    .buttonStyle(.bordered)
-                    .controlSize(.large)
-                    .buttonBorderShape(.capsule)
-                    .tint(isAdded ? .gray : .green)
-                    .padding(.bottom, 20)
-                    .disabled(isAdded)
+                        .buttonStyle(.bordered)
+                        .controlSize(.large)
+                        .buttonBorderShape(.capsule)
+                        .tint(isAdded ? .gray : .green)
+                        .padding(.bottom, 20)
+                        .disabled(isAdded)
                 }
             }
             .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("Tips") {
+                ToolbarItem (placement: .topBarTrailing){
+                    Button ("Tips"){
                         showingTips.toggle()
                     }
-                    .sheet(isPresented: $showingTips) {
+                    .sheet(isPresented: $showingTips){
                         GeneralTipsView()
                     }
                 }
@@ -70,30 +70,6 @@ struct ItineraryAddView: View {
     }
 }
 
-struct OneDayView: View {
-    var body: some View {
-        Text("Day 1")
-    }
-}
-
-struct ThreeDaysView: View {
-    var body: some View {
-        Text("Day 3")
-    }
-}
-
-struct FiveDaysView: View {
-    var body: some View {
-        Text("Day 5")
-    }
-}
-
-struct SevenDaysView: View {
-    var body: some View {
-        Text("Day 7")
-    }
-}
-
 #Preview {
-    ItineraryAddView(destination: Destination(name: "Naples", itinerariesAvailable: []))
+    ItineraryAddView(destination: Destination(name: "Naples", itinerariesAvailable: [Itinerary(name: "Naples", duration: 0, activities: [Activity(name: "Prova1", hourDuration: 1, day: 1, tips: "None")])]))
 }
