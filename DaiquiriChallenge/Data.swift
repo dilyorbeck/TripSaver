e1ba5593948a25e1274123c8baa13696122157aa
Dilyorbek Sharofiddinov <142020869+dilyorbeck@users.noreply.github.com>
2024-10-17 00:17:22 +0200

    Updated Data.swift

--- DaiquiriChallenge/Data.swift
+++ DaiquiriChallenge/Data.swift
@@ -10,22 +10,474 @@ import SwiftUI
 
 class Data: ObservableObject {
     @Published var allDestinations: [Destination] = [
-        Destination(name: "Naples", itinerariesAvailable: [
-            Itinerary(name: "Naples", duration: 1),
-            Itinerary(name: "Naples", duration: 3),
-            Itinerary(name: "Naples", duration: 5),
-            Itinerary(name: "Naples", duration: 7)
-        ]),
-        Destination(name: "Milan", itinerariesAvailable: [
-            Itinerary(name: "Milan", duration: 1),
-            Itinerary(name: "Milan", duration: 3),
-            Itinerary(name: "Milan", duration: 5),
-            Itinerary(name: "Milan", duration: 7)
-        ])
+        
+        Destination(
+            name: "Florence",
+            itinerariesAvailable: [
+                Itinerary(
+                    name: "Florence",
+                    duration: 0,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Florence",
+                    duration: 1,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Florence",
+                    duration: 2,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Florence",
+                    duration: 3,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+            ]
+        ),
+        Destination(
+            name: "Milan",
+            itinerariesAvailable: [
+                Itinerary(
+                    name: "Milan",
+                    duration: 0,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Milan",
+                    duration: 1,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Milan",
+                    duration: 2,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Milan",
+                    duration: 3,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+            ]
+        ),
+        Destination(
+            name: "Naples",
+            itinerariesAvailable: [
+                Itinerary(
+                    name: "Naples",
+                    duration: 0,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Naples",
+                    duration: 1,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Naples",
+                    duration: 2,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Naples",
+                    duration: 3,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+            ]
+        ),
+        Destination(
+            name: "Rome",
+            itinerariesAvailable: [
+                Itinerary(
+                    name: "Rome",
+                    duration: 0,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Rome",
+                    duration: 1,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Rome",
+                    duration: 2,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Rome",
+                    duration: 3,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+            ]
+        ),
+        Destination(
+            name: "Venice",
+            itinerariesAvailable: [
+                Itinerary(
+                    name: "Venice",
+                    duration: 0,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Venice",
+                    duration: 1,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Venice",
+                    duration: 2,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Venice",
+                    duration: 3,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+            ]
+        ),
+        Destination(
+            name: "Verona",
+            itinerariesAvailable: [
+                Itinerary(
+                    name: "Verona",
+                    duration: 0,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Verona",
+                    duration: 1,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Verona",
+                    duration: 2,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+                Itinerary(
+                    name: "Verona",
+                    duration: 3,
+                    activities: [
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        ),
+                        Activity(
+                            name: "Prova 1",
+                            hourDuration: 1,
+                            day: 1,
+                            tips: "None"
+                        )
+                    ]
+                ),
+            ]
+        ),
     ]
+    
     @Published var yourItineraries: [Itinerary] = []
     @Published var archivedItineraries: [Itinerary] = []
-
+    
 }
 
 var sharedData = Data()
