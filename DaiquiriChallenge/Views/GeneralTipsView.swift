e8f03334068bad83baf2fc00baa384f084015745
Dilyorbek Sharofiddinov <142020869+dilyorbeck@users.noreply.github.com>
2024-10-17 14:44:18 +0200

    Updated GeneralTipsView.swift

--- DaiquiriChallenge/Views/GeneralTipsView.swift
+++ DaiquiriChallenge/Views/GeneralTipsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct GeneralTipsView: View {
     @Environment(\.dismiss) var dismiss
     @ObservedObject var myData = sharedData
+    var destination: Destination = Destination(name: "Naples", generalTips: ["A", "B"], itinerariesAvailable: [])
     
     var body: some View {
         NavigationStack {
@@ -17,7 +18,16 @@ struct GeneralTipsView: View {
                 Color.gray
                     .opacity(0.1)
                     .ignoresSafeArea()
-                Text("General Tips")
+                ScrollView{
+                    ForEach(destination.generalTips, id: \.self) {tip in
+                            Text("• \(tip)")
+                                .font(.title3)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.trailing], 20)
+                                .padding(.top, 2)
+                    }
+                }
+                .navigationTitle("Tips for \(destination.name)")
             }
             .toolbar {
                 ToolbarItem (placement: .cancellationAction){
